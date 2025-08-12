5939c04ff2f054db85b60cc98021f2c662453e59
Любовь Николаева <luba@Lubovs-MacBook-Air.local>
2025-08-12 14:59:52 +0300

    Initial commit of frontend project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        AI-Powered Dynamic Routing Engine
+      </h1>
+    </div>
+  )
+}
+
+export default App
